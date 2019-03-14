669217766c47aa2cf6b00f03c2badbad2fa7f39c
Michail Goncharov <miklemv83@gmail.com>
2019-03-14 10:58:54 +0300

    multi pattern url

--- src/main/java/ru/fusionsoft/jwtauth/BaseUrlAuthenticationFilter.java
+++ src/main/java/ru/fusionsoft/jwtauth/BaseUrlAuthenticationFilter.java
@@ -12,20 +12,25 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 import java.util.regex.Pattern;
 
 
 public abstract class BaseUrlAuthenticationFilter extends GenericFilterBean {
 
-    private String urlPattern;
+    private List<String> urlPatterns;
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public BaseUrlAuthenticationFilter(String urlPattern) {
-        this.urlPattern = urlPattern;
+        this.urlPatterns = Collections.singletonList(urlPattern);
+    }
+    public BaseUrlAuthenticationFilter(List<String> urlPatterns) {
+        this.urlPatterns = urlPatterns;
     }
 
-    public String getUrlPattern() {
-        return urlPattern;
+    public List<String> getUrlPatterns() {
+        return urlPatterns;
     }
 
     public void doFilter(ServletRequest request, ServletResponse response,
@@ -34,11 +39,13 @@ public abstract class BaseUrlAuthenticationFilter extends GenericFilterBean {
         if (request instanceof HttpServletRequest) {
             if (SecurityContextHolder.getContext().getAuthentication() == null) {
                 String uri = ((HttpServletRequest) request).getRequestURI().toString();
-                if (Pattern.matches(urlPattern, uri)) {
-
-                    Authentication authentication = authentication(request, response);
-                    if (authentication != null) {
-                        SecurityContextHolder.getContext().setAuthentication(authentication);
+                for (String urlPattern : urlPatterns) {
+                    if (Pattern.matches(urlPattern, uri)) {
+                        Authentication authentication = authentication(request, response);
+                        if (authentication != null) {
+                            SecurityContextHolder.getContext().setAuthentication(authentication);
+                        }
+                        break;
                     }
                 }
             }
